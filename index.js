37a4ee06c098c5d667dda7351ffca7b0f6c9d276
fd98d <edufederipsg@hotmail.com>
2024-05-15 23:00:30 +0700

    hello commit

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-function findMin(nums) {
-  let left = 0;
-  let right = nums.length - 1;
-  while (left < right) {
-    const mid = Math.floor((left + right) / 2);
-    if (nums[mid] > nums[right]) left = mid + 1;
-    else if (nums[mid] < nums[right]) right = mid;
-    else right--;
+function uniquePathsWithObstacles(obstacleGrid) {
+  const m = obstacleGrid.length;
+  const n = obstacleGrid[0].length;
+  const dp = Array.from(Array(m), () => Array(n).fill(0));
+  if (obstacleGrid[0][0] === 0) dp[0][0] = 1;
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (obstacleGrid[i][j] === 1) continue;
+      if (i > 0) dp[i][j] += dp[i - 1][j];
+      if (j > 0) dp[i][j] += dp[i][j - 1];
+    }
   }
-  return nums[left];
+  return dp[m - 1][n - 1];
 }
